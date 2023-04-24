a05c23fadb4a0ca8378b0359307c8127188d1c91
WaiswaDonnie <waiswadonnie@gmail.com>
2023-04-24 20:25:41 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import PickerSelect from 'react-native-picker-select';
+import model1 from './data/model1.json';
+import model2 from './data/model2.json';
 
-export default function App() {
+const App = () => {
+  const [selectedModel, setSelectedModel] = useState(null);
+  const [inputValues, setInputValues] = useState({});
+  const [calculatedValues, setCalculatedValues] = useState({});
+
+  const handleModelSelect = (model) => {
+    setSelectedModel(model);
+    setInputValues({});
+    setCalculatedValues({});
+  };
+
+  const handleInputValueChange = (fieldName, value) => {
+    setInputValues({ ...inputValues, [fieldName]: value });
+  };
+
+  const handleCalculate = () => {
+    if (selectedModel) {
+      const calculated = {};
+      Object.entries(selectedModel.fields).forEach(([fieldName, fieldDef]) => {
+        if (fieldDef.calculate) {
+          const value = eval(fieldDef.calculate);
+          calculated[fieldName] = value;
+        }
+      });
+      setCalculatedValues(calculated);
+    }
+  };
+
+  const renderInputField = (fieldName, fieldDef) => {
+    return (
+      <View key={fieldName}>
+        <Text>{fieldDef.label}</Text>
+        <TextInput
+          value={inputValues[fieldName]}
+          keyboardType={fieldDef.type === 'number' ? 'numeric' : 'default'}
+          onChangeText={(value) => handleInputValueChange(fieldName, value)}
+          editable={!fieldDef.readOnly}
+        />
+      </View>
+    );
+  };
+
+  const renderModelFields = () => {
+    return Object.entries(selectedModel.fields).map(([fieldName, fieldDef]) => {
+      if (fieldDef.calculate) {
+        return null;
+      }
+      return renderInputField(fieldName, fieldDef);
+    });
+  };
+
+  const renderCalculatedField = (fieldName, value) => {
+    return (
+      <View key={fieldName}>
+        <Text>{selectedModel.fields[fieldName].label}</Text>
+        <Text>{value}</Text>
+      </View>
+    );
+  };
+
+  const renderCalculatedFields = () => {
+    if (Object.keys(calculatedValues).length === 0) {
+      return null;
+    }
+    return Object.entries(calculatedValues).map(([fieldName, value]) =>
+      renderCalculatedField(fieldName, value)
+    );
+  };
+
+  const modelOptions = [
+    { label: 'Select a model', value: null },
+    { label: model1.name, value: model1 },
+    { label: model2.name, value: model2 },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <PickerSelect
+        placeholder={{ label: 'Select a model', value: null }}
+        items={modelOptions}
+        value={selectedModel}
+        onValueChange={handleModelSelect}
+      />
+      {selectedModel && (
+        <View>
+          {renderModelFields()}
+          <Button title="Calculate" onPress={handleCalculate} />
+          {renderCalculatedFields()}
+        </View>
+      )}
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+ 
+export default App;
